ab1136acda16991a65eb83348df964ea2da2cbb8
NAKAO Kazuyoshi <nakao4532@gmail.com>
2018-07-01 17:18:06 +0900

    Your first function: Adopting a pet

--- petshop/contracts/Adoption.sol
+++ petshop/contracts/Adoption.sol
@@ -6,4 +6,16 @@ contract Adoption {
     // address: Ethereumのアドレス用の型
     // public: 公開権限(private/public/internal/external)
     address[16] public adopters;
+
+    // adopt: 指定したペットを採用扱いにする
+    // function 関数名(引数) 公開権限 return 返り値の型
+    function adopt(uint petId) public returns (uint) {
+        // require: 変数の確認
+        require(petId >= 0 && petId <= 15);
+
+        // msg.sender: トランザクションの実行者アドレス
+        adopters[petId] = msg.sender;
+
+        return petId;
+    }
 }

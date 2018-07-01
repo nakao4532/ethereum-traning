b74798ee6d3aa2d4f1cbb71a31f64e0f879a01c1
NAKAO Kazuyoshi <nakao4532@gmail.com>
2018-07-01 19:28:24 +0900

    Getting The Adopted Pets and Updating The UI

--- petshop/src/js/app.js
+++ petshop/src/js/app.js
@@ -56,10 +56,23 @@ App = {
     $(document).on('click', '.btn-adopt', App.handleAdopt);
   },
 
+  // ペットの情報を取得し、オーナーがいる場合は、ApdoptボタンをSuccessに変更する
   markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    App.contracts.Adoption.deployed().then(function(instance) {
+      adoptionInstance = instance;
+
+      return adoptionInstance.getAdopters.call();
+    }).then(function(adopters) {
+      for (i = 0; i < adopters.length; i++) {
+        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    });
   },
 
   handleAdopt: function(event) {
